be61d7b6fd4e659ff31e66108090752941cdeb90
Jacob Duncan <jtduncan@gmail.com>
2018-02-03 10:41:52 -0600

    added babel, eslint to proj

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
-var chalk = require('chalk')
+//var chalk = require('chalk') //common js
+import chalk from 'chalk'; // es6
 
-console.log(chalk.green('Starting app in dev mode...'))
+console.log(chalk.green('Starting app in dev mode...')) //eslint-disable-line no-console

84345373f3e78af698e8d11dae2a9be0d306ea69
Jacob Duncan <jtduncan@gmail.com>
2018-02-03 20:21:35 -0600

    mock api (json-server stuff)

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,25 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers(){
   return get('users');
 }
 
+export function deleteUser(userId){
+  return del(`users/${userId}`)
+}
+
 function get(url){
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url){
+  const request = new Request(baseUrl + url, {
+    method: "DELETE"
+  });
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response){

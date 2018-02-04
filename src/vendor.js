856d20a8d3453292922dc2ba8ae5fb69c9f898b1
Jacob Duncan <jtduncan@gmail.com>
2018-02-04 00:55:03 -0600

    added vendor bundling, html minification, hashing to cache bust

---
+++ src/vendor.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+/** contains refs to vendor libs. Use by webpack in prod mode only. vendor libs should change
+ *  less frequently than main.
+ */
+
+ import fetch from 'whatwg-fetch';
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,49 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web', // can be electron or node
   output: {  //will get served from memory
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //generate external css with hash in filename
+    new ExtractTextPlugin("[name].[contenthash].css"),
+    //hashes filename
+    new WebpackMd5Hash(),
+
+    // used to create separate bundle of vendor libs so that they're cached separately
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor"
+    }),
+
+    // Create html file that includes a reference to bundled js
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
     //eliminate duplicate packages
     new webpack.optimize.DedupePlugin(),
     //minify
@@ -23,7 +52,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("css?sourceMap")}
     ]
   }
 }
